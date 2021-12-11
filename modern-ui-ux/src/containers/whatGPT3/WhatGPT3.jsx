2328d5dcdde322e500665f8a21cd7ef55cb1dc10
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-11 11:15:58 +0600

    What GPT3 section's markups

--- modern-ui-ux/src/containers/whatGPT3/WhatGPT3.jsx
+++ modern-ui-ux/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { Features } from "../../components";
 import "./whatgpt3.css";
 
 export default function WhatGPT3() {
-  return <div>What gpt3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Features />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          Possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
 }
