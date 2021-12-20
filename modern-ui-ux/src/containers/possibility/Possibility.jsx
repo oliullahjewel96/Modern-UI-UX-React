5161c1ee94328138e76df885e12d98011a7fce51
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-20 21:38:02 +0600

    Added possibilty section

--- modern-ui-ux/src/containers/possibility/Possibility.jsx
+++ modern-ui-ux/src/containers/possibility/Possibility.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 export default function Possibility() {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The Possibilities are beyond your imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos illum
+          consequuntur iure deleniti itaque expedita unde dolorum enim quod
+          aspernatur?
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 }
--- modern-ui-ux/src/containers/whatGPT3/WhatGPT3.jsx
+++ modern-ui-ux/src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import "./whatgpt3.css";
 
 export default function WhatGPT3() {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT-3"
