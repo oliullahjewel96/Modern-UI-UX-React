8ace2ae7423526fbae5cb352417e23dddd9bdba5
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-21 23:04:05 +0600

    Footer section

--- modern-ui-ux/src/containers/footer/Footer.jsx
+++ modern-ui-ux/src/containers/footer/Footer.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            aperiam, sapiente quos incidunt corrupti velit.
+          </p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy </p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in Touch</h4>
+          <p>Bashundhara Dhaka</p>
+          <p>555-5555-5555 </p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyrigh">
+        <p>&copy; 2021 GPT-3. All rights reserved</p>
+      </div>
+    </div>
+  );
 }
